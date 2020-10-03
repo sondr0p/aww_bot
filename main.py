0ed2fbcd24127cbe575833b3a6543d8e1f4c3c1e
Sidney Son <psndnb@gmail.com>
2020-10-03 10:13:53 -0700

    Setting up twitter stuff

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-from urllib.request import urlopen as uReq
-import requests
 import tweepy
-import time
-import schedule
+import json
 
 def post():
-    # Get auth
-    auth = tweepy.OAuthHandler('consumerkey', 'consumersecret')
-    auth.set_access_token('token', 'tokensecret')
+    twitter_file = open('twitter.json',)
+    twitter_data = json.load(twitter_file)
+    twitter_file.close
 
-    api = tweepy.API(auth)
+    consumerkey = twitter_data["consumerkey"]
+    consumersecret = twitter_data["consumersecret"]
+    token = twitter_data["token"]
+    tokensecret = twitter_data["tokensecret"]
+
+    # Get auth
+    #auth = tweepy.OAuthHandler('consumerkey', 'consumersecret')
+    #auth.set_access_token('token', 'tokensecret')
+
+    #api = tweepy.API(auth)
 
     #override tweepy.StreamListener to add logic to on_status
-    class MyStreamListener(tweepy.StreamListener):
-        def on_status(self, status):
-            print(status.text)
+    #class MyStreamListener(tweepy.StreamListener):
+    #    def on_status(self, status):
+    #        print(status.text)
 
-    myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    #myStreamListener = MyStreamListener()
+    #myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 
     # Tweet
     # api.update_status(title)
 
-schedule.every().day.at("10:00").do(post)
-
-while True:
-    schedule.run_pending()
-    time.sleep(360)
+post()
